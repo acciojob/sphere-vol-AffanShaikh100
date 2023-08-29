18ea20b039fd9ec225289b439cf5350c3e41a471
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-08-29 18:48:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+let radius = document.getElementById("radius");
+let volume = document.getElementById("volume");
+
 function volume_sphere() {
     //Write your code here
-  
+	let inputr = radius.value;
+	let valuetinp = parseInt(inputr);
+	let ans = (4/3) * Math.PI * Math.pow(valuetinp,3);
+	volume.value = ans;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
